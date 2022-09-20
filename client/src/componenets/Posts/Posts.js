36ae66585566d30e44a0b60bee304728bd701a3e
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2022-09-20 20:41:39 +0300

    created redux boler plate

--- client/src/componenets/Posts/Posts.js
+++ client/src/componenets/Posts/Posts.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Post from './Post/Post';
+import useStyles from './styles.js';
+import {useSelector}from 'react-redux';
 
 export default function Posts() {
+    const posts = useSelector((state)=> state.posts);
+    const classes = useStyles(); 
+    console.log(posts);
+    
   return (
     <>
         <h1>POSTS</h1>
